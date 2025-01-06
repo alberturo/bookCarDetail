50a1a5a64cb300d60c19a1a04120a4bcd28e8f1c
Albert Ramirez <alberturo@gmail.com>
2025-01-05 18:19:48 -0600

    before deployment

--- react-vite/src/components/BookCard/BookCard.jsx
+++ react-vite/src/components/BookCard/BookCard.jsx
@@ -1,32 +1,39 @@
-import { useNavigate } from 'react-router-dom';
-import './BookCard.css'
+import { useNavigate } from "react-router-dom";
+import "./BookCard.css";
 import { FaStar } from "react-icons/fa";
-import { useDispatch } from 'react-redux';
-import { thunkRemoveBookFromCollection } from '../../redux/books';
+import { useDispatch } from "react-redux";
+import { thunkRemoveBookFromCollection } from "../../redux/books";
 
-function BookCard({book, collectionId = false}){
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+function BookCard({ book, collectionId = false }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleNav = () => {
-    navigate(`/books/${book.id}/details`)
-  }
+    navigate(`/books/${book.id}/details`);
+  };
 
   const handleDelete = () => {
-    dispatch(thunkRemoveBookFromCollection(collectionId, book.id))
-  }
+    dispatch(thunkRemoveBookFromCollection(collectionId, book.id));
+  };
 
-  return(
+  return (
     <div className="book-card">
-      <img src={`${book.imgUrl}`} onClick={handleNav}/>
+      <img
+        style={{ width: "200px" }}
+        src={`${book.imgUrl}`}
+        onClick={handleNav}
+      />
       {collectionId && <button onClick={handleDelete}>X</button>}
-      <ul className='book-name-author'>
-        <li style={{textWrap:'wrap'}}>{`${book.name}`}</li>
-        <li>{`Score: ${book.avgRating}  `}<FaStar style={{color:'gold'}}/></li>
+      <ul className="book-name-author">
+        <li style={{ textWrap: "wrap" }}>{`${book.name}`}</li>
+        <li>
+          {`Score: ${book.avgRating}  `}
+          <FaStar style={{ color: "gold" }} />
+        </li>
         <li>{`${book.author}`}</li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default BookCard
+export default BookCard;
--- react-vite/src/components/LandingPage/LanguageSlide.jsx
+++ react-vite/src/components/LandingPage/LanguageSlide.jsx
@@ -1,15 +1,21 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const LANGUAGES = {"English": "eng", "Spanish": "sp", "Japanese":"jp"}
+const LANGUAGES = { English: "eng", Spanish: "sp", Japanese: "jp" };
 
-function LanguageSlide({slideInfo}){
-  const navigate = useNavigate()
+function LanguageSlide({ slideInfo }) {
+  const navigate = useNavigate();
   return (
     <div className="img-slide-container">
-      <h2 id="slide-title" style={{color:"white"}}>{slideInfo.lang}</h2>
-      <img onClick={() => navigate(`/books/${LANGUAGES[slideInfo.lang]}`)} src={`${slideInfo.url}`} />
+      <h2 id="slide-title" style={{ color: "#98d4e1", fontFamily: "Delius" }}>
+        {slideInfo.lang}
+      </h2>
+      <img
+        onClick={() => navigate(`/books/${LANGUAGES[slideInfo.lang]}`)}
+        src={`${slideInfo.url}`}
+        style={{ width: "300px" }}
+      />
     </div>
-  )
+  );
 }
 
-export default LanguageSlide
+export default LanguageSlide;
